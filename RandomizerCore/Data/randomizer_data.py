3371acfd397be837e029ffedf34bd171c60fe831
Owen Smart <Owen32Smart@gmail.com>
2025-08-10 14:57:39 -0400

    change data and paths to use pathlib Path instead of os

--- RandomizerCore/Data/randomizer_data.py
+++ RandomizerCore/Data/randomizer_data.py
@@ -1,24 +1,18 @@
 from RandomizerCore.Paths.randomizer_paths import *
-
 import yaml
-import os
 
-VERSION = "0.2.0"
+VERSION = "0.3.0"
 
-with open(os.path.join(RESOURCE_PATH, 'adjectives.txt'), 'r') as f:
+with open(RESOURCE_PATH / 'adjectives.txt', 'r') as f:
     ADJECTIVES = f.read().splitlines()
 
-with open(os.path.join(RESOURCE_PATH, 'characters.txt'), 'r') as f:
+with open(RESOURCE_PATH / 'characters.txt', 'r') as f:
     CHARACTERS = f.read().splitlines()
 
-with open(os.path.join(RESOURCE_PATH, 'changelog.txt'), 'r') as f:
-    full_log = f.read()
-    logs = full_log.split('------')
-    NEW_CHANGES = logs[0]
-    logs.pop(0)
-    OLD_CHANGES = "".join(logs).strip()
+with open(RESOURCE_PATH / 'changelog.txt', 'r') as f:
+    CHANGES = f.read()
 
-with open(os.path.join(DATA_PATH, 'parameters.yml'), 'r') as f:
+with open(DATA_PATH / 'parameters.yml', 'r') as f:
     PARAMS = yaml.safe_load(f)
 
 try:
--- RandomizerCore/Paths/randomizer_paths.py
+++ RandomizerCore/Paths/randomizer_paths.py
@@ -1,24 +1,24 @@
-import os
-import sys
-import appdirs
+from pathlib import Path
+import appdirs, platform
 
-if getattr(sys, "frozen", False):
+try:
+    from sys import _MEIPASS
     RUNNING_FROM_SOURCE = False
-    ROOT_PATH = os.path.dirname(sys.executable)
-    if sys.platform == 'darwin': # mac
-        userdata_path = appdirs.user_data_dir('randomizer', 'Splatoon 3 Randomizer')
-        if not os.path.isdir(userdata_path):
-            os.mkdir(userdata_path)
-        SETTINGS_PATH = os.path.join(userdata_path, 'settings.txt')
-        LOGS_PATH = os.path.join(userdata_path, 'log.txt')
+    ROOT_PATH = Path(_MEIPASS)
+    if platform.system() == 'Darwin': # mac
+        userdata_path = Path(appdirs.user_data_dir('randomizer', 'Splatoon 3 Randomizer'))
+        if not userdata_path.exists():
+            userdata_path.mkdir(parents=True, exist_ok=True)
+        SETTINGS_PATH = userdata_path / 'settings.txt'
+        LOGS_PATH = userdata_path / 'log.txt'
     else:
-        SETTINGS_PATH = os.path.join('.', 'settings.txt')
-        LOGS_PATH = os.path.join('.', 'log.txt')
-else:
+        SETTINGS_PATH = ROOT_PATH / 'settings.txt'
+        LOGS_PATH = ROOT_PATH / 'log.txt'
+except ImportError:
     RUNNING_FROM_SOURCE = True
-    ROOT_PATH = os.path.abspath(os.path.dirname(sys.argv[0]))
-    SETTINGS_PATH = os.path.join(ROOT_PATH, 'settings.txt')
-    LOGS_PATH = os.path.join(ROOT_PATH, 'log.txt')
+    ROOT_PATH = Path(__file__).parent.absolute()
+    SETTINGS_PATH = ROOT_PATH / 'settings.txt'
+    LOGS_PATH = ROOT_PATH / 'log.txt'
 
-DATA_PATH = os.path.join(ROOT_PATH, 'RandomizerCore/Data')
-RESOURCE_PATH = os.path.join(ROOT_PATH, 'RandomizerUI/Resources')
+DATA_PATH = ROOT_PATH / 'RandomizerCore' / 'Data'
+RESOURCE_PATH = ROOT_PATH / 'RandomizerUI' / 'Resources'
