0c6e8c6b0c3cf57dfb69978c8b856f5f19f623d1
Owen Smart <Owen32Smart@gmail.com>
2025-08-15 01:01:26 -0400

    add comments and method descriptions to explain the purpose of the custom widgets

--- RandomizerUI/custom_widgets.py
+++ RandomizerUI/custom_widgets.py
@@ -7,25 +7,33 @@ class RandoComboBox(QComboBox):
     """Custom QComboBox that overrides the showPopup() and hidePopup() methods"""
 
     def __init__(self, parent) -> None:
-        super(RandoComboBox, self).__init__()
-        self.setParent(parent)
+        super(RandoComboBox, self).__init__(parent)
         self.upwards: bool = False
 
 
     def showPopup(self) -> None:
-        """Custom showPopup implementation to move it fully below or above the combobox"""
+        """Custom showPopup implementation to move it fully below or above the combobox
+        
+        This also prevents the popup from showing below or above the window.
+        The contents of a window should stay within the bounds of the window"""
 
         QComboBox.showPopup(self)
         popup: QWidget = self.findChild(QFrame)
-        pos_x = popup.x()
+
+        # Because we went with the 'fusion' style, the currently selected item is in the position of the combobox
+        # This means that the y position of the popup changes depending on the current index
+        # We can use the current index and basic math to place the popup how we want it
         pos_y = popup.y() + (self.height() * (self.currentIndex() + 1))
         if self.upwards:
             pos_y -= (self.height() + popup.height())
-        popup.move(pos_x, pos_y)
+
+        popup.move(popup.x(), pos_y)
 
 
     def hidePopup(self):
-        """Custom hidePopup implementation to reset the explanation text"""
+        """Custom hidePopup implementation to reset the explanation text
+        
+        Without this, the explanation text would continue to show the text for the combobox even after it has been closed"""
 
         QComboBox.hidePopup(self)
         if isinstance(self.window(), QMainWindow):
@@ -34,7 +42,9 @@ class RandoComboBox(QComboBox):
 
 
 class RandoHelpWindow(QMessageBox):
-    """Custom QMessageBox that supports scroll"""
+    """Custom QMessageBox that supports scroll
+    
+    We also use stylesheets to control the minimum window size so that the text is always fully visible"""
 
     def __init__(self, title: str, text: str, with_scroll: bool = False):
         super(RandoHelpWindow, self).__init__()
