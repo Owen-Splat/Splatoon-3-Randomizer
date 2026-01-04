bf81a34f7eb4cd7c17f810167b185f9cfa9fc9b1
Owen-Splat <Owen32Smart@gmail.com>
2026-01-04 03:42:07 -0500

    remove game version from the ui and settings, we will just determine version from romfs

--- RandomizerUI/settings_manager.py
+++ RandomizerUI/settings_manager.py
@@ -14,16 +14,11 @@ def saveSettings(window) -> None:
     if len(seed) > 32:
         seed = seed[:32]
 
-    major_version = window.ui.findSpinBox("MajorVersion").value()
-    minor_version = window.ui.findSpinBox("MinorVersion").value()
-    patch_version = window.ui.findSpinBox("PatchVersion").value()
-
     settings = {
         'RomFS': window.ui.findLineEdit("BaseLine").text(),
         # 'DLC': window.ui.findLineEdit("DLCLine").text(),
         'Output': window.ui.findLineEdit("OutLine").text(),
         'Seed': seed,
-        'Version': f"{major_version}.{minor_version}.{patch_version}",
         'Platform': window.ui.findComboBox("PlatformBox").currentText()[9:].strip(),
         'HeroMode': {},
         # 'SideOrder': {}
@@ -73,14 +68,6 @@ def loadSettings(window, settings) -> None:
                         case 'Platform':
                             window.ui.findComboBox("PlatformBox").setCurrentIndex(
                                 1 if str(settings[k]).lower().strip() == 'emulator' else 0)
-                        case 'Version':
-                            game_version = str(settings[k]).split('.')
-                            major_version = int(game_version[0])
-                            minor_version = int(game_version[1])
-                            patch_version = int(game_version[2])
-                            window.ui.findSpinBox("MajorVersion").setValue(major_version)
-                            window.ui.findSpinBox("MinorVersion").setValue(minor_version)
-                            window.ui.findSpinBox("PatchVersion").setValue(patch_version)
                 except: # if it errors we dont really care why, ignore so it is left at the default value
                     continue
             else:
