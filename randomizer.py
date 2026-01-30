c1a7525e5177572906a8416504a228eaddd43735
Owen Smart <Owen32Smart@gmail.com>
2026-01-30 06:31:37 -0500

    check for windows instead of catching an error

--- randomizer.py
+++ randomizer.py
@@ -14,13 +14,13 @@ signal.signal(signal.SIGINT, interruptHandler)
 
 # Set app id so the custom taskbar icon will show while running from source
 if RUNNING_FROM_SOURCE:
-    try:
-        from ctypes import windll
-        windll.shell32.SetCurrentProcessExplicitAppUserModelID("Splatoon3_Randomizer")
-    except AttributeError:
-        pass # Ignore for versions of Windows before Windows 7
-    except ImportError: # linux
-        pass
+    import platform
+    if platform.system() == "Windows":
+        try:
+            from ctypes import windll
+            windll.shell32.SetCurrentProcessExplicitAppUserModelID("Splatoon3_Randomizer")
+        except AttributeError: # Ignore error on old versions of windows
+            pass
 
 build_icon = "icon.ico"
 if sys.platform == "darwin": # mac
