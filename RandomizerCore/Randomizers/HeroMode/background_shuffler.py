34fb84d53dd0a710dc770b8dc600597fb8a7e340
Owen_Splat <Owen32Smart@gmail.com>
2026-02-24 01:36:27 -0500

    add scaling to crater skyboxes to fix glitchy effect

--- RandomizerCore/Randomizers/HeroMode/background_shuffler.py
+++ RandomizerCore/Randomizers/HeroMode/background_shuffler.py
@@ -1,5 +1,6 @@
 from RandomizerCore.Tools.zs_tools import BYAML, SARC
 from randomizer_paths import DATA_PATH
+import oead
 
 with open(DATA_PATH / "HeroMode" / "backgrounds.txt", "r") as f:
     BACKGROUNDS = f.read().splitlines()
@@ -18,4 +19,6 @@ def randomizeBackground(rng, msn: str, zs_data: SARC) -> None:
         sky = rng.choice(BACKGROUNDS)
         render_data.info["Lighting"]["SkySphere"]["ActorName"] =\
             f"Work/Actor/{sky}.engine__actor__ActorParam.gyml"
+        if "Crater" in sky:
+            render_data.info["lighting"]["SkySphere"]["Scale"] = oead.F32(5.0)
         zs_data.writer.files[renders[0]] = render_data.repack()
