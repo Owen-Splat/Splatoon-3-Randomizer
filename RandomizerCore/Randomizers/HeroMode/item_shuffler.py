c67e96fc25e5e777c2342eeddfa199cd80bc0d0f
Owen_Splat <Owen32Smart@gmail.com>
2026-02-24 20:08:47 -0500

    keep infinite zipcaster can in rocket vanilla, also tweak the drop weights

--- RandomizerCore/Randomizers/HeroMode/item_shuffler.py
+++ RandomizerCore/Randomizers/HeroMode/item_shuffler.py
@@ -4,26 +4,31 @@ import oead, secrets
 # we only include one enemy entry because it gets removed if enemies arent randomized
 # enemies get randomized after the item drops, so it will turn into a different enemy anyway
 DROPS = {
-    "ItemArmor": 2.5,
+    "ItemArmor": 1.5,
     "ItemCanSpecial_Blower": 0.2,
-    "ItemCanSpecial_Chariot": 0.3,
+    "ItemCanSpecial_Chariot": 0.2,
     "ItemCanSpecial_InkStorm": 0.2,
-    "ItemCanSpecial_Jetpack": 0.3,
+    "ItemCanSpecial_Jetpack": 0.2,
     "ItemCanSpecial_MicroLaser": 0.2,
     "ItemCanSpecial_MultiMissile": 0.2,
     "ItemCanSpecial_ShockSonar": 0.2,
     "ItemCanSpecial_Skewer": 0.2,
-    "ItemCanSpecial_SuperHook": 0.3,
-    "ItemCanSpecial_SuperLanding": 0.3,
+    "ItemCanSpecial_SuperHook": 0.2,
+    "ItemCanSpecial_SuperLanding": 0.2,
     "ItemCanSpecial_TripleTornado": 0.2,
     "ItemCanSpecial_UltraShot": 0.2,
     "ItemCanSpecial_UltraStamp": 0.2,
-    # "ItemIkura": 2.5, # no physics and floating in the air looks weird
+    # "ItemIkura": 2.5, # no physics so it floats where it spawns. works fine but looks weird
     "ItemIkuraBottle": 5.0,
-    # "ItemIkuraLarge": 4.0,
-    # "ItemInkBottle": 2.0, # seems to crash the game
+    # "ItemIkuraLarge": 4.0, # grizz power eggs?
+    "ItemInkBottle": 2.0,
     "EnemyTakopter": 4.0
 }
+
+VANILLA_DROPS = (
+    "ItemCardKey",
+    "ItemCanInfinitySpecial_SuperHook"
+)
 
 
 def randomizeItems(thread, level_sarc: SARC) -> None:
@@ -49,13 +54,13 @@ def randomizeItems(thread, level_sarc: SARC) -> None:
                     linked_ids.append(link["Dst"])
 
     # iterate through them and change the object type
-    # keys need to be left vanilla
+    # keys need to be left vanilla, so does the infinite zipcaster can in rocket
     for act in banc.info["Actors"]:
         if not thread.thread_active:
             break
         if act["Hash"] not in linked_ids:
             continue
-        if act["Name"] == "ItemCardKey":
+        if act["Name"] in VANILLA_DROPS:
             continue
         new_item = thread.rng.choices(list(valid_drops.keys()), list(valid_drops.values()))[0]
         act["Gyaml"] = new_item
@@ -83,7 +88,7 @@ def randomizeItems(thread, level_sarc: SARC) -> None:
             continue
         if "Links" in act:
             continue
-        if thread.rng.random() < (1 / 5): # 1/5 chance of no drop
+        if thread.rng.random() < (1 / 4): # 1/4 chance of no drop
             continue
         act["spl__WoodenBoxBancParam"] = {} # remove egg reward
         act["spl__AirBallBancParam"] = {} # remove egg reward
